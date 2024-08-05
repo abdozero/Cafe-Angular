bd9756ca5fd0a38a5e9c08b6651ef52edbdb3048
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-05 12:57:14 +0300

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ProductsComponent } from './Component/products/products.component';
+import { OrdersComponent } from './Component/orders/orders.component';
+import { AboutComponent } from './Component/about/about.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { ProfileComponent } from './Component/profile/profile.component';
+import { CartComponent } from './Component/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"products", component:ProductsComponent},
+  {path:"orders", component:OrdersComponent},
+  {path:"about", component:AboutComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"profile", component:ProfileComponent},
+  {path:"cart", component:CartComponent}
+];
