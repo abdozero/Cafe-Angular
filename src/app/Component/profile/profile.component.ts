9dff4a534b100999904131355cf826185a6564e8
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-11 17:52:26 +0300

    Implement profile component

--- src/app/Component/profile/profile.component.ts
+++ src/app/Component/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { OrdersHistoryComponent } from './orders-history/orders-history.component';
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [EditProfileComponent, OrdersHistoryComponent, AppComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
