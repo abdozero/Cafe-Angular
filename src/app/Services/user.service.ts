43cc6c1d4aef729df379a16f2537f35a1c8a2b2b
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-14 19:56:36 +0300

    CheckUserExist, AddUser, and check password in other functions

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, of, switchMap } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, throwError } from 'rxjs';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,26 +9,63 @@ import { map, Observable, of, switchMap } from 'rxjs';
 export class UserService {
   DB_URL = "http://localhost:3000/users";
 
-  isPasswordCorrect = false;
-  editUserByIdResponce: any;
   constructor(public myHttp: HttpClient) { }
-  GetUserByName(name:string, password:string){
-    let id = name.toLowerCase().trim().replace(/[\s\t]+/g, "-")
-    return this.myHttp.get(this.DB_URL+"/"+id);
+
+  CheckUserExist(id: string): Observable<boolean>{
+    return this.myHttp.get(this.DB_URL+"/"+id).pipe(
+      map(() => true),
+      catchError(error => {
+        if (error.status === 404) {
+          return of(false);
+        }
+        else {return throwError(error);}
+      })
+    );
   }
 
-  GetUserById(id: string){
-    return this.myHttp.get(this.DB_URL+"/"+id);
+  AddUser(user: User){
+    return this.myHttp.post(this.DB_URL, user);
   }
 
-  verifyPassword(id: string, password: string | null): Observable<boolean> {
+  VerifyPassword(id: string, password: string | null): Observable<boolean> {
     return this.myHttp.get<any>(this.DB_URL + "/" +id).pipe(
       map(user => user.password === password)
     );
   }
 
+  GetUserByName(name:string, password:string){
+    let id = name.toLowerCase().trim().replace(/[\s\t]+/g, "-");
+    return this.VerifyPassword(id, password).pipe(
+      switchMap(isVerified => {
+        if (isVerified)
+        {
+          return this.myHttp.get(this.DB_URL+"/"+id);
+        }
+        else
+        {
+          return of({ error: 'Password verification failed' });
+        }
+      })
+    );
+  }
+
+  GetUserById(id: string, password:string){
+    return this.VerifyPassword(id, password).pipe(
+      switchMap(isVerified => {
+        if (isVerified)
+        {
+          return this.myHttp.get(this.DB_URL+"/"+id);
+        }
+        else
+        {
+          return of({ error: 'Password verification failed' });
+        }
+      })
+    );
+  }
+
   EditUserById(id:string, password:string | null, update: any): Observable<any> {
-    return this.verifyPassword(id, password).pipe(
+    return this.VerifyPassword(id, password).pipe(
       switchMap(isVerified => {
         if (isVerified)
         {
