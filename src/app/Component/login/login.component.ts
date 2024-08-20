fd51776519ea2ba8a9d315eb090e9c82eac4fd35
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-20 16:27:16 +0300

    implement for test authorization

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../Services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule],
+  providers: [UserService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private userService: UserService, private http: HttpClientModule){}
+  username = "";
+  password = "";
+  login(){
+    this.userService.Login(this.username, this.password).subscribe(error=>console.log(error));
+  }
 }
