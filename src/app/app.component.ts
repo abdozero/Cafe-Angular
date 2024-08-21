77e9b1a30b9d6a3027b30a040338aceb87d24142
AliaaAhmed612 <alyaaahmed612@gmail.com>
2024-08-21 22:09:03 +0300

    Add kfklklkl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { HeaderComponent } from './Component/header/header.component';
-import { FooterComponent } from "./Component/footer/footer.component";
+import { FooterComponent } from './Component/footer/footer.component';
 import { HomeComponent } from './Component/home/home.component';
-import { AboutComponent } from "./Component/about/about.component";
+import { AboutComponent } from './Component/about/about.component';
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
+import { ProductsComponent } from './Component/products/products.component';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,15 @@ import { Component } from '@angular/core';
   imports: [
     RouterOutlet,
     HomeComponent,
+    ProductsComponent,
     AboutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Restaurant';
-  BrandName: string = "Brand Name";
+  BrandName: string = 'Brand Name';
 }
