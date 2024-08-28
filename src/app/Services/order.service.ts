59c386dd3e6553d99005d61c64793a83cede6270
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-28 23:57:33 +0300

    create order service

--- src/app/Services/order.service.ts
+++ src/app/Services/order.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(
+    private Http: HttpClient,
+  ) {}
+
+  DB_URL = 'http://localhost:3003/orders';
+
+  GetAllOrders():Observable<Order[]>{
+    return this.Http.get<Order[]>(this.DB_URL);
+  }
+
+  GetOreders(userId: string): Observable<Order[]>{
+    return this.Http.get<Order[]>(this.DB_URL+'?userId='+userId);
+  }
+
+  AddOrders(order: Order){
+    return this.Http.post(this.DB_URL, order);
+  }
+
+  ChangeStatus(id: string, status: string){
+    return this.Http.patch(this.DB_URL + '/' + id, {status: status});
+  }
 }
