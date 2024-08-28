4799e8b62e24df50bb245fd60461c623e669216d
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-28 12:17:01 +0300

    remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { FooterComponent } from './Component/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { UserService } from './Services/user.service';
-import { ProductsComponent } from './Component/products/products.component';
-import { DetailsComponent } from './Component/details/details.component';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +11,6 @@ import { DetailsComponent } from './Component/details/details.component';
     RouterOutlet,
     HeaderComponent,
     FooterComponent,
-    ProductsComponent,
-    DetailsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
