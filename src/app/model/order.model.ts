a123bfed3407e62c3a5be38298f27a635fba119c
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-29 00:03:35 +0300

    create order model

---
+++ src/app/model/order.model.ts
@@ -0,0 +1,15 @@
+export interface Order {
+  id: string;
+  userId: string;
+  datetime: string;
+  products: OrderProduct[];
+  total: number;
+  status: string;
+}
+
+interface OrderProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
