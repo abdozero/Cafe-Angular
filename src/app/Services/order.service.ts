4240ad261f07317cce80fd7d02a91b84c2eb40e4
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-08-30 14:27:54 +0300

    add GetAllOrdersWithUserInfo

--- src/app/Services/order.service.ts
+++ src/app/Services/order.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { Order } from '../model/order.model';
+import { UserService } from './user.service';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +12,27 @@ export class OrderService {
 
   constructor(
     private Http: HttpClient,
+    private userService: UserService
   ) {}
 
   DB_URL = 'http://localhost:3003/orders';
 
   GetAllOrders():Observable<Order[]>{
     return this.Http.get<Order[]>(this.DB_URL);
+  }
+
+  GetAllOrdersWithUserInfo(): Observable<any[]> {
+    const users$ = this.userService.GetAllUsers();
+    const orders$ = this.GetAllOrders();
+
+    return forkJoin([users$, orders$]).pipe(
+      map(([users, orders]) => {
+        return orders.map((order: Order) => ({
+          ...order,
+          user: users.find((user: User) => user.id === order.userId)
+        }));
+      })
+    );
   }
 
   GetOreders(userId: string): Observable<Order[]>{
